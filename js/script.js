a8e186a06ca7833e4215aa95f977532ccb23b0c2
Akash Yadav <ay790184@gmail.com>
2024-08-14 15:46:45 +0530

    create a js file
    create a js file and code it of addition of two numbers which is give by user

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// alert("Hello");
+function add(){
+    var num1 = document.getElementById("num1").value;
+    alert(num1);
+    var num2 = document.getElementById("num2").value;
+    alert(num2);
+
+
+    var add = parseInt(num1)+parseInt(num2);
+    alert(add);
+}
