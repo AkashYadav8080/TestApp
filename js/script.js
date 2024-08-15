c88542fbd02d43043d7e955a6ba8c9d3768d7a54
Akash Yadav <ay790184@gmail.com>
2024-08-15 14:36:43 +0530

    styling of webpage
    new styling of webpage

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 // alert("Hello");
 function add(){
     var num1 = document.getElementById("num1").value;
-    alert(num1);
+    // alert(num1);
     var num2 = document.getElementById("num2").value;
-    alert(num2);
-
-
+    // alert(num2);
     var add = parseInt(num1)+parseInt(num2);
-    alert(add);
+    // alert(add);
+    document.getElementById("ans").innerHTML = add;
 }
